e7c1816441ac50ab43bd508f266cd5c57dc1b772
Shams Mamadjonov <sh.mamadjon@gmail.com>
2022-05-04 09:15:35 +0500

    small changes

--- src/app/components/main/projects/projects.component.ts
+++ src/app/components/main/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProjectService} from "../../../_services/project.service";
 
 import * as mapboxgl from 'mapbox-gl';
@@ -13,30 +13,30 @@ export class ProjectsComponent implements OnInit {
 
   maps: mapboxgl.map;
 
-  constructor(private projectService: ProjectService) { }
+  constructor(private projectService: ProjectService) {
+  }
 
   ngOnInit(): void {
-    this.showMaps();
     this.getProjectsInfo();
   }
 
   showMaps(): void {
-   mapboxgl.accessToken = environment.mapboxKey;
-   let maps = new mapboxgl.Map({
+    mapboxgl.accessToken = environment.mapboxKey;
+    let maps = new mapboxgl.Map({
       container: 'maps-mapbox',
-     style: 'mapbox://styles/mapbox/streets-v11',
-     center: [43.37478851081459, 34.78427250153706],
-     zoom: 2
-   });
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [43.37478851081459, 34.78427250153706],
+      zoom: 2
+    });
   }
 
   getProjectsInfo(): void {
     this.projectService.getProjectsInfo()
       .subscribe(data => {
         console.log(data);
+        this.showMaps();
       });
   }
-
 
 
 }
